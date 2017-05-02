8c2d70541e1749776a827b62a0a3d6ae39ce7704
pavithracv <pavi231295@gmail.com>
2017-05-02 10:35:50 +0530

    Update ResponceForm.java

--- ResponceForm.java
+++ ResponceForm.java
@@ -197,7 +197,7 @@ public ResponceForm(String s,String msg) {
                 String question="";
                 String option1="";
                 String option2="";
-                String option3="";
+             //   String option3="";
                 String option4="";
                 String answer="";
                 int mark=0;
